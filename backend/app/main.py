2c29bb9e3c720e7090b7345b0393691f793fe959
Alex Tovshchyk <takiid0@proton.me>
2025-06-05 14:13:02 -0500

    Connected db to backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from app.db import init_db, get_session
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    try:
+        init_db()
+        print("Database schema initialized successfully.", flush=True)
+    except Exception as e:
+        print("Failed to initialize DB:", e, flush=True)
+        raise e
 
 @app.get("/")
 def read_root():
