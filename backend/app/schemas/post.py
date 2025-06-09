f0f287591b01574b8c4bc78843476b786239ab34
Alex Tovshchyk <takiid0@proton.me>
2025-06-09 02:25:02 -0500

    Created post model and create, read schemas

---
+++ backend/app/schemas/post.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+import time
+import calendar
+
+
+class PostCreate(BaseModel):
+    user_id: int
+    content: str
+    createdAt: str = str(calendar.timegm(time.gmtime()))
+    disabled = False
+
+
+class PostRead(BaseModel):
+    id: int
+    user_id: int
+    content: str
+    createdAt: str | None = None
+
+    class Config:
+        orm_mode = True
